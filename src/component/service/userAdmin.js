a1e499cd3dd9d1e8287c959a27e4bb456dd72fbb
blineder <ben@bloomzone.com>
2025-08-14 17:27:39 +0300

    hardcode api name (#24)

--- src/component/service/userAdmin.js
+++ src/component/service/userAdmin.js
@@ -1,11 +1,25 @@
-import { API } from 'aws-amplify';
+//import { API } from 'aws-amplify'; // Keep this import for now, but it will be removed soon.
+//import awsExports from '../../aws-exports';
 
-const apiName = 'usersAdmin';
-const path = '/users';
+//const apiName = 'usersAdmin';
+//const path = '/users';
 
 async function callUserAdminApi(payload, successMessage, errorMessage) {
+//  console.log('awsExports.aws_cloud_logic_custom: ', awsExports.aws_cloud_logic_custom);
+//  console.log('path: ', path);
+//  console.log('path.substring(1): ', path.substring(1));
   try {
-    const response = await API.post(apiName, path, payload);
+    const apiUrl = 'https://rvz67ef1yc.execute-api.us-east-1.amazonaws.com/master/users';
+//    const apiUrl = `${awsExports.aws_cloud_logic_custom[0].endpoint}/${path.substring(1)}`; // Construct the full API URL
+    console.log('apiUrl: ', apiUrl);
+    const response = await fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+//      body: JSON.stringify(payload.body), // Assuming payload.body contains the actual data
+    });
     console.log(successMessage, response);
     return response;
   } catch (error) {
