9aea048d4ce867b70100b4f8410cbf28f990ed96
ben <ben@bloomzone.com>
2025-08-15 15:37:18 +0300

    add to awsconfig required key

--- src/index.js
+++ src/index.js
@@ -8,6 +8,14 @@ import awsconfig from "./aws-exports";
 import { ToastContainer } from "react-toastify";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+if (!("aws_cloud_logic_custom" in awsconfig)) {
+  awsconfig["aws_cloud_logic_custom"] = [{
+    "name": "usersAdmin",
+    "endpoint": "https://rvz67ef1yc.execute-api.us-east-1.amazonaws.com/master",
+    "region": "us-east-1"
+  }];
+}
+console.log('awsconfig:', awsconfig);
 Amplify.configure(awsconfig);
 root.render(
   <>
