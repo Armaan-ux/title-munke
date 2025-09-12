d34b35211ea8fea010295936d70c7995474efcd3
ajay <ajaygaur@creativebuffer.com>
2025-09-12 11:45:22 +0530

    update in aws-exports

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { UserProvider } from "./context/usercontext";
-import { Amplify, Auth } from "aws-amplify";
-import awsconfig from "./aws-exports";
+import { Amplify } from "aws-amplify";
+// import awsconfig from "./aws-exports";
+import { awsmobile } from "./aws-exports";
 import { ToastContainer } from "react-toastify";
 // import { Buffer } from 'buffer'
 // import process from 'process/browser'
@@ -15,16 +16,16 @@ import { ToastContainer } from "react-toastify";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-console.log('awsconfig --> ', awsconfig);
-if (!("aws_cloud_logic_custom" in awsconfig)) {
-  awsconfig["aws_cloud_logic_custom"] = [{
+console.log('awsconfig --> ', awsmobile);
+if (!("aws_cloud_logic_custom" in awsmobile)) {
+  awsmobile["aws_cloud_logic_custom"] = [{
     "name": "usersAdmin",
     "endpoint": "https://nz8vshfeah.execute-api.us-east-1.amazonaws.com/master",
     "region": "us-east-1",
     "authorizationType": "AMAZON_COGNITO_USER_POOLS",
   }];
 }
-Amplify.configure(awsconfig);
+Amplify.configure(awsmobile);
 root.render(
   <>
     <UserProvider>
