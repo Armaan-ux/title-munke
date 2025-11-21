767cb362ed4598089f194c0c5e244d7481050ef4
cb-official <team@creativebuffer.com>
2025-11-21 15:42:30 +0530

    fix invoice billing details.

--- src/components/Modal/InvoiceModal.jsx
+++ src/components/Modal/InvoiceModal.jsx
@@ -14,7 +14,7 @@ export function InvoiceModal({ open, onClose, invoice }) {
   // if (!open) return null;
   // if(!invoice) return null
   const isAgentPayment = invoice?.plans?.find(plan => plan?.priceName === "Agent Seat Price")?.quantity > 0;
-  
+  console.log("is  agent payment", isAgentPayment)
   return (
     <div ref={invoiceRef} className={isDownloading ? "flex justify-center [&>div]:!w-[70%]" : ""}>
       <div className="rounded-2xl bg-white p-6 shadow-lg flex flex-col gap-4">
@@ -82,6 +82,7 @@ export function InvoiceModal({ open, onClose, invoice }) {
           <div className="bg-[#fdf8f5] text-sm divide-y">
             {invoice?.plans?.map(plan => {
               if(isAgentPayment && plan?.priceName === "Monthly Subscription Price") return null;
+              if(!isAgentPayment && plan?.priceName === "Agent Seat Price") return null;
               return <div className="grid grid-cols-5 items-center px-3 py-2" key={plan?.priceId}>
                 <div className="col-span-2">
                   <p className="font-medium text-[#581b1b]">{plan?.priceName}</p>
